31f60e77a8a62353020229e08037852d747c8c61
The BaK <ilbak@ilbak.it>
2022-12-27 19:21:30 +0100

    Update iltofagame.py
    Aggiornati margini del percorso del player

--- iltofagame.py
+++ iltofagame.py
@@ -4,6 +4,8 @@ import pygame
 import random
 pygame.init()
 
+WIDTH = 300
+HEIGHT = 500
 SCHERMO = pygame.display.set_mode((288, 512))
 
 
@@ -17,31 +19,35 @@ player = pygame.image.load("./images/alien.png")
 
 # =================================================================================== SETUP
 
-def setup():
-    global status, playerx, playery, playerdir, speed, steer, lives, level, score
-    # status 0 intro 1 menu 2 ingioco
-    status=0
+global status, playerx, playery, playerdir, speed, steer, lives, level, score
+# status 0 intro 1 menu 2 ingioco
+status=0
 
-    # player
-    playerx = 200
-    playery = 200
-    playerdir = True
-    speed = 30
-    steer = 4
+# player
+playerx = 200
+playery = 400
+playerdir = True
+speed = 30
+steer = 4
 
-    lives = 0
-    level = 1
-    score = 0
+lives = 0
+level = 1
+score = 0
+
+# margine sinistro e margine destro
+stageleft = 20
+stageright = WIDTH - 50
 
 
 
 def draw():
-        
+
     pass
-    
+
 def update():
     global status, playerx, playery, playerdir, speed, steer, lives, level, score
-    
+    global stageleft, stageright
+
 # =================================================================================== Aggiorna Schermo
 
     SCHERMO.blit(background, (0, 0))
@@ -55,12 +61,10 @@ def update():
         playerdir= False
     else:
         playerdir = True
-        
-    if playerdir == True:
+
+    if playerdir == True and playerx < stageright:
         playerx += 4
-    else:
+    if playerdir == False and playerx > stageleft:
         playerx -= 4
-    
-setup()
 
 
